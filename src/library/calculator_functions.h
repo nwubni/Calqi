60ea9423e344945e71cbfc2d5aaf210a671136ff
Nakuru Wubni <nwubni@gmail.com>
2026-03-04 08:59:58 +0100

    Restructured expression evaluation

--- src/library/calculator_functions.h
+++ src/library/calculator_functions.h
@@ -11,8 +11,6 @@ double multiply(const double &a, const double &b);
 double divide(const double &a, const double &b);
 double abs(const double &a);
 bool isOperator(const std::string &op);
-bool isAdditionOrSubtraction(const std::string &op);
-bool isMultiplicationOrDivision(const std::string &op);
 double evaluate(std::vector<double> &numbers,
                 std::vector<std::string> &operators);
 } // namespace calqi
